34685e5fde97322e716b56c80738ad37e664843f
HTY2003 <randumbperson2003@gmail.com>
2019-04-30 03:10:15 +0800

    probably can't do thumb detection

--- image_segment.py
+++ image_segment.py
@@ -1,6 +1,7 @@
 import cv2
 import imutils
 import numpy as np
+import math
 from sklearn.metrics import pairwise
 class BackgroundSubtract:
     def __init__(self, bgframe):
@@ -8,11 +9,13 @@ class BackgroundSubtract:
         self.bg = bgframe
 
     def foreground(self, frame):
-        frame = cv2.GaussianBlur(frame, (7, 7), 0)
-        bg =  frame * self.aWeight + self.bg * (1 - self.aWeight)
-        diff = cv2.absdiff(bg.astype(np.uint8),frame)
-
-
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        bg =  gray * self.aWeight + self.bg * (1 - self.aWeight)
+        diff = cv2.absdiff(bg.astype(np.uint8),gray)
+        _, diff = cv2.threshold(diff, 23,255,cv2.THRESH_BINARY)
+        diff = np.repeat(diff[:, :, np.newaxis], 3, axis=2)
+        return frame * diff * diff
 
 class ColorSegment:
     def calibrate(self, frame):
@@ -20,9 +23,9 @@ class ColorSegment:
         x, y, _ = frame.shape
         _, r, b = frame[int(x/2), int(y/2)]
         self.rmin = r - 10
-        self.rmax = r + 10
+        self.rmax = r + 12
         self.bmin = b - 10
-        self.bmax = b + 10
+        self.bmax = b + 12
 
     def extract(self, frame):
         minrange = np.array([0, self.rmin, self.bmin])
@@ -30,56 +33,141 @@ class ColorSegment:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
         mask = cv2.inRange(frame, minrange, maxrange)
         return mask
-    
+
     def denoise(self, frame):
-        kernel = np.ones((20,20),np.uint8)
-        blur = cv2.blur(frame,(5,5))
-        erosion = cv2.morphologyEx(blur, cv2.MORPH_OPEN, kernel)
-        return erosion
+        kernel = np.ones((15,15),np.uint8)
+        erosion = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
+        reblur = cv2.blur(erosion,(35,35))
+        _, final = cv2.threshold(reblur, 100, 255, cv2.THRESH_BINARY)
+        return final
     
+    def calculateFingers(self, res,drawing):  # -> finished bool, cnt: finger count
+        #  convexity defect
+        hull = cv2.convexHull(res, returnPoints=False)
+        if len(hull) > 3:
+            defects = cv2.convexityDefects(res, hull)
+            if type(defects) != type(None):  # avoid crashing.   (BUG not found)
+
+                cnt = 0
+                for i in range(defects.shape[0]):  # calculate the angle
+                    s, e, f, d = defects[i][0]
+                    start = tuple(res[s][0])
+                    end = tuple(res[e][0])
+                    far = tuple(res[f][0])
+                    a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                    b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                    c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                    angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+                    if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+                        cnt += 1
+                        cv2.circle(drawing, far, 8, [211, 84, 0], -1)
+                return True, cnt
+        return False, 0
+
     def contour(self, frame):
+        frame2 = np.repeat(frame[:, :, np.newaxis], 3, axis=2)
         (contours, _) = cv2.findContours(frame,
-                                            cv2.RETR_EXTERNAL,
-                                            cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours) == 0:
+                                        cv2.RETR_TREE,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+        length = len(contours)
+        if length == 0:
             return
         else:
-            segmented = max(contours, key=cv2.contourArea)
-            chull = cv2.convexHull(segmented)
-            extreme_top    = tuple(chull[chull[:, :, 1].argmin()][0])
-            extreme_bottom = tuple(chull[chull[:, :, 1].argmax()][0])
-            extreme_left   = tuple(chull[chull[:, :, 0].argmin()][0])
-            extreme_right  = tuple(chull[chull[:, :, 0].argmax()][0])
+            maxArea = -1
+            for i in range(length):  # find the biggest contour (according to area)
+                temp = contours[i]
+                area = cv2.contourArea(temp)
+                if area > maxArea:
+                    maxArea = area
+                    ci = i
 
-            cX = int((extreme_left[0] + extreme_right[0]) / 2)
-            cY = int((extreme_top[1] + extreme_bottom[1]) / 2)
-            distance = pairwise.euclidean_distances([(cX, cY)], Y=[extreme_left, extreme_right, extreme_top, extreme_bottom])[0]
-            maximum_distance = distance[distance.argmax()]
+            res = contours[ci]
+            hull = cv2.convexHull(res)
+            drawing = np.zeros(frame.shape, np.uint8)
+            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
+            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
+
+            isFinishCal,cnt = self.calculateFingers(res,drawing)
+            '''cnts = max(contours, key=cv2.contourArea)
+            hull = cv2.convexHull(cnts)
+            hull2 = cv2.convexHull(cnts,returnPoints = False)
+            defects = cv2.convexityDefects(cnts,hull2)
             
-            radius = int(0.8 * maximum_distance)
-            circumference = (2 * np.pi * radius)
-
-
-            circular_roi = np.zeros(frame.shape[:2], dtype="uint8")
-            print(cX, cY)
-            cv2.circle(circular_roi, (cX, cY), radius, 255, 1)
+            FarDefect = []
+            for i in range(defects.shape[0]):
+                s,e,f,d = defects[i,0]
+                start = tuple(cnts[s][0])
+                end = tuple(cnts[e][0])
+                far = tuple(cnts[f][0])
+                FarDefect.append(far)
+                cv2.line(frame,start,end,[0,255,0],1)
+                cv2.circle(frame,far,10,[100,255,255],3)
             
-            circular_roi = cv2.bitwise_and(frame, frame, mask=circular_roi)
-            (contours, _) = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            moments = cv2.moments(cnts)
+            
+            if moments['m00']!=0:
+                cx = int(moments['m10']/moments['m00']) # cx = M10/M00
+                cy = int(moments['m01']/moments['m00']) # cy = M01/M00
+            centerMass=(cx,cy)    
+            
+            cv2.circle(frame,centerMass,7,[100,0,255],2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame,'Center',tuple(centerMass),font,2,(255,255,255),2)     
+            
+            distanceBetweenDefectsToCenter = []
+            for i in range(0,len(FarDefect)):
+                x =  np.array(FarDefect[i])
+                centerMass = np.array(centerMass)
+                distance = np.sqrt(np.power(x[0]-centerMass[0],2)+np.power(x[1]-centerMass[1],2))
+                distanceBetweenDefectsToCenter.append(distance)
+            
+            sortedDefectsDistances = sorted(distanceBetweenDefectsToCenter)
+            AverageDefectDistance = np.mean(sortedDefectsDistances[0:2])
+        
+            finger = []
+            for i in range(0,len(hull)-1):
+                if (np.absolute(hull[i][0][0] - hull[i+1][0][0]) > 80) or ( np.absolute(hull[i][0][1] - hull[i+1][0][1]) > 80):
+                    if hull[i][0][1] <500 :
+                        finger.append(hull[i][0])
+            
+            finger =  sorted(finger,key=lambda x: x[1])   
+            fingers = finger[0:5]
+            
+            fingerDistance = []
+            for i in range(0,len(fingers)):
+                distance = np.sqrt(np.power(fingers[i][0]-centerMass[0],2)+np.power(fingers[i][1]-centerMass[0],2))
+                fingerDistance.append(distance)
+            
+            result = 0
+            for i in range(0,len(fingers)):
+                if fingerDistance[i] > AverageDefectDistance+130:
+                    result += 1'''
+            '''segmented = max(contours, key=cv2.contourArea)
+            chull2 = cv2.convexHull(segmented, returnPoints=False)
+            defects = cv2.convexityDefects(segmented,chull2)
+            for i in range(defects.shape[0]):
+                s,e,f,d = defects[i,0]
+                start = tuple(segmented[s][0])
+                end = tuple(segmented[e][0])
+                far = tuple(segmented[f][0])
+                cv2.line(frame2,start,end,[0,255,0],5)
+                cv2.circle(frame2,far,5,[0,0,255],-1)
+            cv2.imshow('img',frame2)
+            cv2.waitKey(0)'''
+            
+            return cnt
 
-            count = 0
-
-            for c in contours:
-                (x, y, w, h) = cv2.boundingRect(c)
-                count += 1
-            return count
-
-framergb = np.uint8(cv2.imread("images/11.JPG"))
-framergb = cv2.resize(framergb, (0,0), fx=0.4, fy = 0.4)
-a = ColorSegment()
-a.calibrate(framergb)
-b = a.extract(framergb)
-b = a.denoise(b)
-cv2.imshow("hi",b)
+bg = cv2.imread("images/0.JPG", 0)
+framergb = cv2.imread("images/4.JPG")
+bg = cv2.resize(bg, (0,0), fx=0.2, fy = 0.2)
+framergb = cv2.resize(framergb, (0,0), fx=0.2, fy = 0.2)
+a = BackgroundSubtract(bg)
+b = a.foreground(framergb)
+c = ColorSegment()
+c.calibrate(framergb)
+d = c.extract(b)
+d = c.denoise(d)
+e = np.repeat(d[:, :, np.newaxis], 3, axis=2)
+cv2.imshow("hi",e*e*framergb)
 cv2.waitKey(0)
-print(a.contour(b))
+print(c.contour(d))
